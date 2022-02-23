80f9fb044ab718a7591f19a36e83e07d5bb7aa05
JulioEI <julioesparzaibanez@gmail.com>
2022-02-23 18:14:24 +0100

    transfer verbose_functions print_time_verbose' and Tee to flush prints into txt

---
+++ neural_manifold/general_utils/verbose_functions.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 23 14:46:59 2022
+
+@author: Usuario
+"""
+import timeit
+from datetime import datetime
+
+
+class Tee(object):
+    def __init__(self, *files):
+        self.files = files
+    def write(self, obj):
+        for f in self.files:
+            f.write(obj)
+            f.flush() # If you want the output to be visible immediately
+    def flush(self) :
+        for f in self.files:
+            f.flush()
+            
+            
+def print_time_verbose(local_starttime, global_starttime= None):
+    
+    if isinstance(global_starttime, type(None)):
+        print(f'\t{datetime.now():%Y-%m-%d %H:%M}', end=' ')
+        print(f'- Running Time: {timeit.default_timer()-local_starttime:.4f} s')
+    else:
+        print(f'\t{datetime.now():%Y-%m-%d %H:%M}', end=' ')
+        print(f'- Running Time: {timeit.default_timer()-local_starttime:.4f} s', end= ' ')
+        print(f'({timeit.default_timer()-global_starttime:.4f} s)')
+    
+    
