36542714353b05bb9992c38228f560882f2e7733
JulioEI <julioesparzaibanez@gmail.com>
2022-05-09 12:46:15 +0200

    'handle_inputs' removed from __init__.py importing since it has been deprecated in favor of new decorator 'check_inputs_for_pd'

--- neural_manifold/general_utils/__init__.py
+++ neural_manifold/general_utils/__init__.py
@@ -8,10 +8,6 @@ Created on Fri Feb 18 13:44:10 2022
 from .translators import *
 from .load_files import *
 from .plot_utils import *
-from .handle_inputs import *
 from .verbose_functions import *
 from .data_preprocessing import *
-
-#import neural_manifold.decoders.decoder_functions
-#import neural_manifold.translators.translators
 
